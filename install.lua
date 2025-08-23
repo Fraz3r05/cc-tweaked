4165b582eb5290f5e98dfc2223b9bf0511b9976f
Fraz3r05 <96805712+Fraz3r05@users.noreply.github.com>
2025-08-23 21:59:24 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,1 +1,29 @@
-print("hello there")
+-- install: wget run https://raw.githubusercontent.com/Fraz3r05/cc-tweaked/refs/heads/main/install.lua
+-- update:  install
+
+local scripts = {
+    "install.lua",
+    "createFile.lua",
+    "importFile.lua",
+}
+
+local files = {
+    "helloPastebin.lua"
+}
+
+print("installing pastebin content...\n")
+
+for i=1, table.getn(scripts) do
+    local name = scripts[i]
+    
+    shell.execute("rm", name)
+    shell.execute("wget", "https://raw.githubusercontent.com/Fraz3r05/cc-tweaked/refs/heads/main/" .. name)
+end
+
+print("\n")
+
+for i=1, table.getn(files) do
+    local name = files[i]
+    
+    shell.execute("createFile", name)
+end
