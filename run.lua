2d7f8eee266d24268ea34e43bf5e0cd34fedb96e
Yanik Adamson <yanik.adamson@gmail.com>
2025-11-21 19:16:07 +0100

    fix run.lua

--- run.lua
+++ run.lua
@@ -1,3 +1,3 @@
 local args = { ... }
 local fileName = args[1]
-shell.execute("wget", "run", "https://raw.githubusercontent.com/Fraz3r05/cc-tweaked/refs/heads/main/build/" .. fileName)
+shell.execute("wget", "run", "https://raw.githubusercontent.com/Fraz3r05/cc-tweaked/refs/heads/main/build/" .. fileName .. ".lua")
